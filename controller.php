af224d952b674eea36184b28d8e400c17ccf1d67
Fabian Bitter <fabian@bitter.de>
2024-09-23 18:15:01 +0200

    Update

--- controller.php
+++ controller.php
@@ -17,7 +17,7 @@ use Concrete\Theme\Concrete\PageTheme;
 class Controller extends Package
 {
     protected $pkgHandle = 'bitter_theme';
-    protected $pkgVersion = '3.1.2';
+    protected $pkgVersion = '3.1.3';
     protected $appVersionRequired = '9.2.0';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderRegistries = [
